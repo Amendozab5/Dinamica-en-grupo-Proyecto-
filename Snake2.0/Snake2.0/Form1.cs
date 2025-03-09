428bb3c22c413f24cb86a3baa8a03358aa4afe0f
Ernesto835 <elunam4@uteq.edu.ec>
2025-03-09 14:55:08 -0500

    Add files via upload
    Se corrigió el problema de colisión con el borde derecho, asegurando que la serpiente detecte correctamente los límites del formulario.

---
+++ Snake2.0/Snake2.0/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Snake2._0
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> Lista = new List<PictureBox>();
+        int tamañoPiezaPrincipal = 26;
+        PictureBox Comida = new PictureBox();
+        string direccion = "right";
+
+        public Form1()
+        {
+            InitializeComponent();
+            iniciarJuego();
+            tmTiempo.Start();
+        }
+
+        public void iniciarJuego()
+        {
+            direccion = "right";
+            tmTiempo.Interval = 200;
+            lblPuntos.Text = "0";
+            Lista.Clear();
+
+            for (int i = 2; i >= 0; i--)
+            {
+                int posX = (i * tamañoPiezaPrincipal) + 70;
+                int posY = 80;
+
+                crearSnake(Lista, this, posX, posY);
+            }
+
+            crearComida();
+        }
+
+        public void crearSnake(List<PictureBox> listaPelota, Form formulario, int posicionx, int posiciony)
+        {
+            PictureBox pb = new PictureBox
+            {
+                Location = new Point(posicionx, posiciony),
+                Image = Properties.Resources.bodys,
+                BackColor = Color.Transparent,
+                SizeMode = PictureBoxSizeMode.AutoSize
+            };
+
+            listaPelota.Add(pb);
+            formulario.Controls.Add(pb);
+        }
+
+        private void crearComida()
+        {
+            Random rnd = new Random();
+            int maxX = (this.ClientSize.Width / tamañoPiezaPrincipal) * tamañoPiezaPrincipal - tamañoPiezaPrincipal;
+            int maxY = (this.ClientSize.Height / tamañoPiezaPrincipal) * tamañoPiezaPrincipal - tamañoPiezaPrincipal;
+
+            int enterox = rnd.Next(0, maxX / tamañoPiezaPrincipal) * tamañoPiezaPrincipal;
+            int enteroy = rnd.Next(0, maxY / tamañoPiezaPrincipal) * tamañoPiezaPrincipal;
+
+            PictureBox pb = new PictureBox
+            {
+                Location = new Point(enterox, enteroy),
+                Image = Properties.Resources.apple,
+                BackColor = Color.Transparent,
+                SizeMode = PictureBoxSizeMode.AutoSize
+            };
+
+            if (Comida != null)
+            {
+                this.Controls.Remove(Comida);
+            }
+
+            Comida = pb;
+            this.Controls.Add(pb);
+        }
+
+        private void tmTiempo_Tick(object sender, EventArgs e)
+        {
+            int nx = Lista[0].Location.X;
+            int ny = Lista[0].Location.Y;
+
+            for (int i = Lista.Count - 1; i > 0; i--)
+            {
+                Lista[i].Location = Lista[i - 1].Location;
+            }
+
+            switch (direccion)
+            {
+                case "right":
+                    nx += tamañoPiezaPrincipal;
+                    Lista[0].Image = Properties.Resources.headright;
+                    break;
+                case "left":
+                    nx -= tamañoPiezaPrincipal;
+                    Lista[0].Image = Properties.Resources.headleft;
+                    break;
+                case "up":
+                    ny -= tamañoPiezaPrincipal;
+                    Lista[0].Image = Properties.Resources.headup;
+                    break;
+                case "down":
+                    ny += tamañoPiezaPrincipal;
+                    Lista[0].Image = Properties.Resources.headdown;
+                    break;
+            }
+
+            Lista[0].Location = new Point(nx, ny);
+
+            verificarColision();
+            verificarColisionComida();
+
+            this.Refresh();
+        }
+
+        private void verificarColision()
+        {
+      
+            if (Lista.Count >= 4)
+            {
+                for (int i = 3; i < Lista.Count; i++)
+                {
+                    if (Lista[0].Bounds.IntersectsWith(Lista[i].Bounds))
+                    {
+                        gameOver();
+                        return;
+                    }
+                }
+            }
+
+            if (Lista[0].Left < 0 ||
+                Lista[0].Right > this.ClientSize.Width ||
+                Lista[0].Top < 0 ||
+                Lista[0].Bottom > this.ClientSize.Height)
+            {
+                gameOver();
+                return;
+            }
+        }
+
+        private void verificarColisionComida()
+        {
+            if (Lista[0].Bounds.IntersectsWith(Comida.Bounds))
+            {
+                this.Controls.Remove(Comida);
+
+                if (tmTiempo.Interval > 50)
+                {
+                    tmTiempo.Interval -= 10;
+                }
+
+                int puntos = Convert.ToInt32(lblPuntos.Text);
+                lblPuntos.Text = (puntos + 1).ToString();
+
+                crearComida();
+
+                int nuevaX = Lista[Lista.Count - 1].Location.X;
+                int nuevaY = Lista[Lista.Count - 1].Location.Y;
+
+                crearSnake(Lista, this, nuevaX, nuevaY);
+            }
+        }
+
+        private void gameOver()
+        {
+            tmTiempo.Stop();
+            gameOver gameOverForm = new gameOver();
+            gameOverForm.ShowDialog();
+        }
+
+        public void reiniciarJuego()
+        {
+            tmTiempo.Stop();
+
+            foreach (var segmento in Lista.ToList())
+            {
+                if (this.Controls.Contains(segmento))
+                {
+                    this.Controls.Remove(segmento);
+                }
+            }
+
+            Lista.Clear();
+
+            if (Comida != null && this.Controls.Contains(Comida))
+            {
+                this.Controls.Remove(Comida);
+                Comida.Dispose();
+            }
+
+            iniciarJuego();
+            tmTiempo.Start();
+        }
+
+        private void moverPieza(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && direccion != "down")
+            {
+                direccion = "up";
+            }
+            else if (e.KeyCode == Keys.Down && direccion != "up")
+            {
+                direccion = "down";
+            }
+            else if (e.KeyCode == Keys.Left && direccion != "right")
+            {
+                direccion = "left";
+            }
+            else if (e.KeyCode == Keys.Right && direccion != "left")
+            {
+                direccion = "right";
+            }
+        }
+    }
+}
---
+++ Snake2.0/Snake2.0/gameOver.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Snake2._0
+{
+    partial class gameOver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnSalir = new Panel();
+            btnRestart = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.BackgroundImage = Properties.Resources.GAME_OVER;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(btnSalir);
+            panel1.Controls.Add(btnRestart);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(535, 453);
+            panel1.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            btnSalir.BackgroundImage = Properties.Resources.EXIT;
+            btnSalir.BackgroundImageLayout = ImageLayout.Stretch;
+            btnSalir.Location = new Point(307, 247);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(139, 54);
+            btnSalir.TabIndex = 1;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // btnRestart
+            // 
+            btnRestart.BackgroundImage = Properties.Resources.RESTART;
+            btnRestart.BackgroundImageLayout = ImageLayout.Stretch;
+            btnRestart.Location = new Point(91, 247);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(139, 54);
+            btnRestart.TabIndex = 0;
+            btnRestart.Click += btnRestart_Click;
+            // 
+            // gameOver
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(535, 453);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "gameOver";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "gameOver";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel btnRestart;
+        private Panel btnSalir;
+    }
+}
